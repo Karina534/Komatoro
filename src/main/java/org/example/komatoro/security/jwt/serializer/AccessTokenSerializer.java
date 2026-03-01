0d2271da33dda615598f34a114442021ec609ff0
Karina534 <smirnova7.04@mail.ru>
2026-03-01 17:26:00 +0300

    made default factory and serializers for tokens

---
+++ src/main/java/org/example/komatoro/security/jwt/serializer/AccessTokenSerializer.java
@@ -0,0 +1,55 @@
+package org.example.komatoro.security.jwt.serializer;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.example.komatoro.security.jwt.JwtToken;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@Slf4j
+public class AccessTokenSerializer implements Function<JwtToken, String> {
+    private final JWSSigner jwsSigner;
+
+    @Getter
+    @Setter
+    private JWSAlgorithm jwsAlgorithm = JWSAlgorithm.HS256;
+
+    public AccessTokenSerializer(JWSSigner jwsSigner, JWSAlgorithm jwsAlgorithm) {
+        this.jwsSigner = jwsSigner;
+        this.jwsAlgorithm = jwsAlgorithm;
+    }
+
+    public AccessTokenSerializer(JWSSigner jwsSigner) {
+        this.jwsSigner = jwsSigner;
+    }
+
+    @Override
+    public String apply(JwtToken jwtToken) {
+        SignedJWT signedJWT = new SignedJWT(
+                new JWSHeader.Builder(jwsAlgorithm).keyID(jwtToken.id().toString()).build(),
+                new JWTClaimsSet.Builder()
+                        .jwtID(jwtToken.id().toString())
+                        .subject(jwtToken.subject())
+                        .issueTime(Date.from(jwtToken.createdAt()))
+                        .claim("authorities", jwtToken.authorities())
+                        .build()
+        );
+
+        try {
+            signedJWT.sign(this.jwsSigner);
+            return signedJWT.serialize();
+        } catch (JOSEException exception){
+            log.error(exception.getMessage(), exception);
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/org/example/komatoro/security/jwt/serializer/RefreshTokenSerializer.java
@@ -0,0 +1,58 @@
+package org.example.komatoro.security.jwt.serializer;
+
+import com.nimbusds.jose.EncryptionMethod;
+import com.nimbusds.jose.JWEAlgorithm;
+import com.nimbusds.jose.JWEEncrypter;
+import com.nimbusds.jose.JWEHeader;
+import com.nimbusds.jwt.EncryptedJWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.example.komatoro.security.jwt.JwtToken;
+
+import java.util.function.Function;
+
+@Slf4j
+public class RefreshTokenSerializer implements Function<JwtToken, String> {
+    private final JWEEncrypter encrypter;
+
+    @Getter
+    @Setter
+    private JWEAlgorithm algorithm = JWEAlgorithm.DIR;
+    private EncryptionMethod encryptionMethod = EncryptionMethod.A256GCM;
+
+    public RefreshTokenSerializer(JWEEncrypter encrypter) {
+        this.encrypter = encrypter;
+    }
+
+    public RefreshTokenSerializer(JWEEncrypter encrypter, JWEAlgorithm algorithm, EncryptionMethod encryptionMethod) {
+        this.encrypter = encrypter;
+        this.algorithm = algorithm;
+        this.encryptionMethod = encryptionMethod;
+    }
+
+    @Override
+    public String apply(JwtToken jwtToken) {
+        EncryptedJWT encryptedJWT = new EncryptedJWT(
+                new JWEHeader.Builder(algorithm, encryptionMethod)
+                        .keyID(jwtToken.id().toString())
+                        .build(),
+                new JWTClaimsSet.Builder()
+                        .jwtID(jwtToken.id().toString())
+                        .subject(jwtToken.subject())
+                        .issueTime(jwtToken.createdAt() == null ? null : java.util.Date.from(jwtToken.createdAt()))
+                        .claim("authorities", jwtToken.authorities())
+                        .build()
+        );
+
+        try {
+            encryptedJWT.encrypt(encrypter);
+            return encryptedJWT.serialize();
+        } catch (Exception exception) {
+            log.error(exception.getMessage(), exception);
+        }
+        return null;
+    }
+}
