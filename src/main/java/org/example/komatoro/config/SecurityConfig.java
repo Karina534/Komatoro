c3525be5f773dc71d969f92e3f3800e86a35f90d
Karina534 <smirnova7.04@mail.ru>
2026-03-11 15:38:16 +0300

    added doc

--- src/main/java/org/example/komatoro/config/SecurityConfig.java
+++ src/main/java/org/example/komatoro/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/users/login", "/api/users/registration").permitAll()
                         .requestMatchers("/login.html", "/home.html").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
